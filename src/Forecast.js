6160aae4e36b05df3658584259767854b811f98b
Alicia <72704070+aazzara@users.noreply.github.com>
2021-02-07 14:07:07 -0500

    Add future forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -2,5 +2,48 @@ import React from "react";
 import "./Forecast.css";
 
 export default function Forecast() {
-  return <div className="row future" id="forecast"></div>;
+  return <div className="row future" id="forecast">
+    <div class="col range">
+      <span class="time-range">Sun 16:00</span>
+      <br/>
+      <img class="forecastIcon" src="http://openweathermap.org/img/wn/10d@2x.png" alt="icon" /> 
+      <br />
+      <span class="tempOne">83</span>ºF
+    </div>
+    <div class="col range">
+      <span class="time-range">Sun 16:00</span>
+      <br/>
+      <img class="forecastIcon" src="http://openweathermap.org/img/wn/10d@2x.png" alt="icon" /> 
+      <br />
+      <span class="tempOne">83</span>ºF
+    </div>
+    <div class="col range">
+      <span class="time-range">Sun 16:00</span>
+      <br/>
+      <img class="forecastIcon" src="http://openweathermap.org/img/wn/10d@2x.png" alt="icon" /> 
+      <br />
+      <span class="tempOne">83</span>ºF
+    </div>
+    <div class="col range">
+      <span class="time-range">Sun 16:00</span>
+      <br/>
+      <img class="forecastIcon" src="http://openweathermap.org/img/wn/10d@2x.png" alt="icon" /> 
+      <br />
+      <span class="tempOne">83</span>ºF
+    </div>
+    <div class="col range">
+      <span class="time-range">Sun 16:00</span>
+      <br/>
+      <img class="forecastIcon" src="http://openweathermap.org/img/wn/10d@2x.png" alt="icon" /> 
+      <br />
+      <span class="tempOne">83</span>ºF
+    </div>
+    <div class="col range">
+      <span class="time-range">Sun 16:00</span>
+      <br/>
+      <img class="forecastIcon" src="http://openweathermap.org/img/wn/10d@2x.png" alt="icon" /> 
+      <br />
+      <span class="tempOne">83</span>ºF
+    </div>
+  </div>;
 }
