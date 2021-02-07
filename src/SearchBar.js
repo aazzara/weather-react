6308ff6947e2253f287ab1c244b4e5979d545342
Alicia <72704070+aazzara@users.noreply.github.com>
2021-02-07 14:15:21 -0500

    Add search bar functionality

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from 'axios';
 import "./SearchBar.css";
 
 export default function SearchBar() {
+  
+let [city, setCity] = useState("");
+let [message, setmessage] = useState("");
+function updateCity(event) {
+  setCity(event.target.value);
+  }
+
+function handleClick(event) {
+  event.preventDefault();
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e57aed5a3752290f9e3c0dd1d0ad914d&units=Metric
+`;
+  axios.get(url).then(getData);
+  }
+function getData(response) {
+  
+  alert(response.data.main.temp)
+}
   return (
     <div className="search-bar">
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleClick}>
         <input
           className="col-6"
           id="changeCity"
           type="text"
+          onChange={updateCity}
           placeholder="Change city"
           autocomplete="off"
         />
